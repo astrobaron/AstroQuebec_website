c6e776bb51de93beed6e352117cba6ecedebcb3a
astrobaron <frederique.baron@umontreal.ca>
2026-09-22 13:17:39 -0400

    Use public CSV for UdeM seminar updates

--- scripts/update_seminars.sh
+++ scripts/update_seminars.sh
@@ -7,7 +7,7 @@ cd "$ROOT_DIR"
 echo "[seminars] refreshing from the two seminar sources..."
 python3 scripts/import_seminars.py \
   --url "https://docs.google.com/spreadsheets/d/1ZFOU0qNz010exBnI2UC_IALXu3Kdp9gDFC_SuSLUScs/export?format=csv&gid=0" \
-  --url "https://udemontreal-my.sharepoint.com/:x:/g/personal/david_lafreniere_umontreal_ca/IQDF7eQ5xo_uR4oiO99xV0NuAW1wxSKhLMBEbsuuyIx337I?e=qFSnfL"
+  --url "https://docs.google.com/spreadsheets/d/e/2PACX-1vTQuijtqP317H2sYk84mriq9OTQhW624Jwn0RH5nv1OHdKsKTYggxKFnax9GAjG2dkT8OxH7825VrSW/pub?output=csv"
 
 echo "[seminars] rebuilding site..."
 hugo --quiet
